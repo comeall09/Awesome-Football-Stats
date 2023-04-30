6f95393314d7cb5fbfb6cc2b5bd63d206eed6df0
k.khamidov <kamolkhamidov200002@gmail.com>
2023-05-01 00:34:44 +0500

    feat: add base commands/scenes classes & configure bot

---
+++ src/commands/index.ts
@@ -0,0 +1,4 @@
+export { StartCommand } from './start.command';
+export { LiveMatchesScene } from './liveMatches.scene';
+export { TodayMatchesScene } from './todayMatches.scene';
+export { TournamentsScene } from './tournaments.scene';
